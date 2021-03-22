106f55684d1bdfe1754ff0285cc775f85d5fcf06
Ramdani Kurnia <adiatma85@gmail.com>
2021-03-22 16:52:10 +0700

    Experiment with new layout in article-index

--- resources/views/Admin/Article/index-article.blade.php
+++ resources/views/Admin/Article/index-article.blade.php
@@ -7,7 +7,7 @@
 'pageTitle' => 'Article',
 'preBreadcrumbs' => [
 'Home' => route('admin.index'),
-'Articles' => route('articles.index')
+'Articles' => route('admin.articles.index')
 ],
 'activeItem' => 'List Article'
 ])
@@ -34,13 +34,40 @@ $itemName = 'title-article';
                 <div class="card-body">
                     <div class="row mb-2">
                         <div class="col-12">
-                            <a href="{{route('articles.create')}}">
+                            <a href="{{route('admin.articles.create')}}">
                                 <button type="button" class="btn btn-primary">
                                     Create new Article
                                 </button>
                             </a>
                         </div>
                     </div>
+                    {{-- Experiment View --}}
+                    {{-- <section id="indexes">
+                        <div class="container">
+                            <div class="row">
+                                @foreach ($articles as $article)
+                                <div class="col-lg-4 mb-2">
+                                    <div class="card">
+                                        <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                                            alt="" class="card-img-top">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{$article->name}}</h5>
+                                            <p class="card-text">@php
+                                                echo substr($article->content, 0, 40).(strlen($article->content) > 40 ?
+                                                '...' : '');
+                                                @endphp
+                                            </p>
+                                            <a href="" class="btn btn-outline-success btn-sm">Edit</a>
+                                            <a href="" class="btn btn-outline-danger btn-sm"><i class="far fa-heart"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </section> --}}
+
+                    {{-- Back up old --}}
                     <table id="main-table" class="table table-bordered table-striped">
                         <thead>
                             <tr>
@@ -55,44 +82,44 @@ $itemName = 'title-article';
                             @foreach ($articles as $article)
                             <tr>
                                 <td>{{$article->name}}</td>
-                                <td>
-                                    @php
-                                    echo substr($article->content, 0, 40).(strlen($article->content) > 40 ? '...' : '');
-                                    @endphp
-                                </td>
-                                <td>{{Carbon::parse($article->created_at)->formatLocalized('%A %d %B %Y')}}</td>
-                                <td>{{$article->counter}}</td>
-                                <td>
-                                    <div class="row">
-                                        <div class="col">
-                                            <a
-                                                href="{{route('articles.edit', ['article' => str_replace(' ', '-', $article->name)])}}">
-                                                <button type="button" class="btn btn-warning">
-                                                    <i class="far fa-edit"></i>
-                                                </button>
-                                            </a>
-                                        </div>
-                                        <div class="col">
+                    <td>
+                        @php
+                        echo substr($article->content, 0, 40).(strlen($article->content) > 40 ? '...' : '');
+                        @endphp
+                    </td>
+                    <td>{{Carbon::parse($article->created_at)->formatLocalized('%A %d %B %Y')}}</td>
+                    <td>{{$article->counter}}</td>
+                    <td>
+                        <div class="row">
+                            <div class="col">
+                                <a
+                                    href="{{route('articles.edit', ['article' => str_replace(' ', '-', $article->name)])}}">
+                                    <button type="button" class="btn btn-warning">
+                                        <i class="far fa-edit"></i>
+                                    </button>
+                                </a>
+                            </div>
+                            <div class="col">
 
-                                            <button type="button" class="btn btn-danger delete-butt-conf"
-                                                data-toggle="modal" data-target="#delete-modal"
-                                                data-artId="{{$article->id}}" data-artName="{{$article->name}}">
-                                                <i class="fas fa-trash"></i>
-                                            </button>
+                                <button type="button" class="btn btn-danger delete-butt-conf" data-toggle="modal"
+                                    data-target="#delete-modal" data-artId="{{$article->id}}"
+                                    data-artName="{{$article->name}}">
+                                    <i class="fas fa-trash"></i>
+                                </button>
 
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                        <tfoot>
-                            <th>Nama Artikel</th>
-                            <th>Konten Artikel</th>
-                            <th>Last updated</th>
-                            <th>Total dikunjungi</th>
-                            <th>Action</th>
-                        </tfoot>
+                            </div>
+                        </div>
+                    </td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                    <tfoot>
+                        <th>Nama Artikel</th>
+                        <th>Konten Artikel</th>
+                        <th>Last updated</th>
+                        <th>Total dikunjungi</th>
+                        <th>Action</th>
+                    </tfoot>
                     </table>
                 </div>
             </div>
