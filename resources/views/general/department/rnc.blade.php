1ed5aaf1a02b41a2d9a4d6df01ac49beb53a8e1c
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2021-04-18 23:35:15 +0700

    department templating + styling

---
+++ resources/views/general/department/rnc.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.department')
+
+@section('title')
+Relation and Creative
+@endsection
+
+@section('description')
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius labore delectus aliquam molestiae laboriosam aliquid. Voluptatibus voluptate, laborum fugit, minima alias incidunt dolore placeat asperiores ab, odit sit! Quis, dolor?
+@endsection
+
+@section('picture-title')
+{{asset('images/misc/department/rnc.png')}}
+@endsection
+
+@section('picture-1')
+{{asset('images/misc/department/career-preparation.png')}}
+@endsection
+
+@section('picture-2')
+{{asset('images/misc/department/feel-it.png')}}
+@endsection
+
+@section('picture-3')
+{{asset('images/misc/department/n-it.png')}}
+@endsection
+
---
+++ resources/views/layouts/department.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+<article class="container">
+    <section class="department__heading">
+        <h1 class="department__heading-title">@yield('title')</h1>
+        <div class="department__heading-sub">
+            <img class="department__heading-sub-img" src="@yield('picture-title')" alt="">
+            <p class="department__heading-sub-description">@yield('description')</p>
+        </div>
+    </section>
+
+    <section class="department__content">
+        <h1 class="department__content-title">Proker</h1>
+        <div class="department__content-group">
+            <img src="
+            @if(View::hasSection('picture-1'))
+                @yield('picture-1')
+            @else
+                {{asset('images/misc/department/empty.png')}}
+            @endif
+            " alt="">
+            <img src="
+            @if(View::hasSection('picture-2'))
+                @yield('picture-2')
+            @else
+                {{asset('images/misc/department/empty.png')}}
+            @endif
+            " alt="">
+            <img src="
+            @if(View::hasSection('picture-3'))
+                @yield('picture-3')
+            @else
+                {{asset('images/misc/department/empty.png')}}
+            @endif
+            " alt="">
+            <img src="
+            @if(View::hasSection('picture-4'))
+                @yield('picture-4')
+            @else
+                {{asset('images/misc/department/empty.png')}}
+            @endif
+            " alt="">
+            <img src="
+            @if(View::hasSection('picture-5'))
+                @yield('picture-5')
+            @else
+                {{asset('images/misc/department/empty.png')}}
+            @endif
+            " alt="">
+            <img src="
+            @if(View::hasSection('picture-6'))
+                @yield('picture-6')
+            @else
+                {{asset('images/misc/department/empty.png')}}
+            @endif
+            " alt="">
+        </div>
+    </section>
+</article>
+@endsection
+
+@section('custom-script')
+@endsection
