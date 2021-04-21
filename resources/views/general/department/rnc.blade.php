d17c0bd0957710197d493011c5ce6e9c874d2652
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2021-04-22 05:24:05 +0700

    rnc, group routing, restyling

--- resources/views/general/department/rnc.blade.php
+++ resources/views/general/department/rnc.blade.php
@@ -5,22 +5,25 @@ Relation and Creative
 @endsection
 
 @section('description')
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius labore delectus aliquam molestiae laboriosam aliquid. Voluptatibus voluptate, laborum fugit, minima alias incidunt dolore placeat asperiores ab, odit sit! Quis, dolor?
+Relation and Creative Bureau merupakan biro yang bertanggung jawab atas segala pengelolaan media sosial yang ada di EMT. Selain itu juga bertanggung jawab dalam menjembatani antara pihak EMTI dengan eksternal dan bertanggung jawab dalam membantu biro ataupun departemnen lain dalamn memberikan sarana penyaluran informasi dan publikasi.
 @endsection
 
 @section('picture-title')
-{{asset('images/misc/department/rnc.png')}}
+{{asset('images/misc/department/rnc/rnc.png')}}
 @endsection
 
 @section('picture-1')
-{{asset('images/misc/department/career-preparation.png')}}
+{{asset('images/misc/department/rnc/company_profile.png')}}
 @endsection
 
 @section('picture-2')
-{{asset('images/misc/department/feel-it.png')}}
+{{asset('images/misc/department/rnc/tips_trick.png')}}
 @endsection
 
 @section('picture-3')
-{{asset('images/misc/department/n-it.png')}}
+{{asset('images/misc/department/rnc/titalks.png')}}
 @endsection
 
+@section('picture-4')
+{{asset('images/misc/department/rnc/trivia.png')}}
+@endsection
