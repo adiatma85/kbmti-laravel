789b9a9942a086795f1979330de50dcb858c4a14
nashihulibad <alamin.ibad@gmail.com>
2021-04-23 21:07:00 +0700

    develop department page

---
+++ resources/views/general/department/entre.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.department')
+
+@section('title')
+Entrepeneurship
+@endsection
+
+@section('description')
+Entrepeneurship Bureau merupakan biro yang bertanggungjawab dalam menunjang internal EMTI dari segi finansial dengan mencari sumber dana potensi dari usaha bisnis maupun kemitraan yang memanfaatkan implementasi ilmu Teknologi Informasi maupun non-TI. Selain itu juga membentuk KBMTI agar memiliki jiwa entrepreneureship.
+@endsection
+
+@section('picture-title')
+{{asset('images/misc/department/entre/entre.png')}}
+@endsection
+
+@section('picture-1')
+{{asset('images/misc/department/entre/bussines-class.png')}}
+@endsection
+
+@section('picture-2')
+{{asset('images/misc/department/entre/jaket-himpunan.png')}}
+@endsection
+
+@section('picture-3')
+{{asset('images/misc/department/entre/takjil.png')}}
+@endsection
+
+
