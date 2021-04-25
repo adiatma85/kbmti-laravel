82fb12d7b90fec4a85e686f90f43ea22276cce6d
kobulwidodo <fadhilhan01@gmail.com>
2021-04-25 14:56:08 +0700

    add rnd and advo

---
+++ resources/views/general/department/advo.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.department')
+
+@section('title')
+Research and Development
+@endsection
+
+@section('description')
+Research and Development Bureau merupakan departemen yang bertanggung jawab mengakomodasi dan mengembangkan potensi mahasiswa dalam bidang keilmuan serta menampung minat bakat mahasiswa di ranah Teknologi Informasi.
+@endsection
+
+@section('picture-title')
+{{asset('images/misc/department/advokasi/advo.png')}}
+@endsection
+
+@section('picture-1')
+{{asset('images/misc/department/advokasi/crisis center.png')}}
+@endsection
+
+@section('picture-2')
+{{asset('images/misc/department/advokasi/info advo.png')}}
+@endsection
+
+@section('picture-3')
+{{asset('images/misc/department/advokasi/info beasiswa.png')}}
+@endsection
+
+@section('picture-4')
+{{asset('images/misc/department/advokasi/opentalk.png')}}
+@endsection
+
+@section('picture-5')
+{{asset('images/misc/department/advokasi/stalk.png')}}
+@endsection
---
+++ resources/views/general/department/rnd.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.department')
+
+@section('title')
+Research and Development
+@endsection
+
+@section('description')
+Research and Development Bureau merupakan departemen yang bertanggung jawab mengakomodasi dan mengembangkan potensi mahasiswa dalam bidang keilmuan serta menampung minat bakat mahasiswa di ranah Teknologi Informasi.
+@endsection
+
+@section('picture-title')
+{{asset('images/misc/department/RnD/rnd.png')}}
+@endsection
+
+@section('picture-1')
+{{asset('images/misc/department/RnD/ngobar.png')}}
+@endsection
+
+@section('picture-2')
+{{asset('images/misc/department/RnD/bank soal.png')}}
+@endsection
+
+@section('picture-3')
+{{asset('images/misc/department/RnD/fun fact.png')}}
+@endsection
+
+@section('picture-4')
+{{asset('images/misc/department/RnD/info lomba.png')}}
+@endsection
+
+@section('picture-5')
+{{asset('images/misc/department/RnD/OSI.png')}}
+@endsection
+
+@section('picture-6')
+{{asset('images/misc/department/RnD/sinau bareng.png')}}
+@endsection
+
+@section('picture-7')
+{{asset('images/misc/department/RnD/web event.png')}}
+@endsection
