5da40f7da17ea05af7c1aa3fe195a041d574332f
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2021-05-07 22:04:41 +0700

    fix content bpmti section

--- resources/views/general/profile/bpmti.blade.php
+++ resources/views/general/profile/bpmti.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
     <article class="container">
-        <section class="profile__heading">
-            @include('include.profile.bpmti-top')
-        </section>
+        @include('include.profile.bpmti-top')
     </article>
 @endsection
 
