43fc0709b0cebe3fbd671e9ccdd1f221042452ee
nashihulibad <alamin.ibad@gmail.com>
2021-06-23 13:50:36 +0700

    adding emti page

---
+++ resources/views/include/profile/emti-top.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.profile-top')
+
+@section('title')
+EMTI
+@endsection
+
+@section('picture-title')
+{{asset('images/misc/profile/bpmti/emti-header.png')}}
+@endsection
+
+@section('picture-side')
+{{asset('images/misc/profile/bpmti/emti-img.png')}}
+@endsection
+
+@section('visi')
+“Mewujudkan Eksekutif Mahasiswa Teknologi Informasi sebagai wadah eksplorasi karya dan solid merangkul dengan dasar kekeluargaan demi terciptanya aksi nyata”
+@endsection
+
+@section('misi')
+"lorem ipsum"
+@endsection
